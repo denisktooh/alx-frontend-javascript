5189dac5ea406b1b977bcd2dffaea89e6569ed28
denisktooh <199378976+denisktooh@users.noreply.github.com>
2025-11-09 00:51:10 +0300

    Namespace & Declaration merging

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,21 +1,19 @@
-import { Java, Teacher as JavaTeacher } from './subjects/Java';
-import { Cpp, Teacher as CppTeacher} from './subjects/Cpp';
-import { React, Teacher as ReactTeacher } from './subjects/React';
+import { Cpp, Teacher as Teacher} from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
 
 const cpp = new Cpp();
-const cTeacher: CppTeacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
+const cTeacher: Teacher = { firstName: 'John', lastName: 'Doe', experienceTeachingC: 10 };
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 const java = new Java();
-const jTeacher: JavaTeacher = { firstName: 'Jane', lastName: 'Smith', experienceTeachingJava: 0 };
-java.setTeacher(jTeacher);
+java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
 const react = new React();
-const rTeacher: ReactTeacher = { firstName: 'Emily', lastName: 'Johnson', experienceTeachingReact: 5 };
-react.setTeacher(rTeacher);
+react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
